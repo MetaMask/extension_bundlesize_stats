512faab13d7c209f45338f04d28af236a669a4a2
jpuri <jyotipuri@gmail.com>
2022-09-13 22:02:45 +0530

    fixes in data files

--- stats/bundle_size_data.js
+++ stats/bundle_size_data.js
@@ -1,10 +1,4 @@
 const data = {
- 9016b82c730dff8a1de24aa28632e23b89b2f473: 
-{
-  "background": 10206902,
-  "ui": 13879803,
-  "timestamp": 1662987757667
-}, 
  '5bb3c93fca75ec9a8982d12823753e57c0d15c91': 
 {
   "background": 4589316,
